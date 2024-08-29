495f7df7082e093bdc89acc1918b067cf8b4abde
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-08-29 11:19:59 +0100

    add ex5

--- ex5.js
+++ ex5.js
@@ -1,12 +1,30 @@
 let allCourses = [
   {
-    nom: "Javascript",
+    titre: "Javascript",
     niveau: "débutant",
     votes: 0,
   },
   {
-    nom: "Php",
+    titre: "Php",
     niveau: "intermédiaire",
     votes: 0,
   },
 ];
+
+let codeAinserer = "";
+
+for (const course of allCourses) {
+  codeAinserer = codeAinserer.concat(
+    `<ul>
+        <li>${course.titre}</li>
+        <li>${course.niveau}</li>
+        <li>${course.votes}</li>
+        <button onclick="updateVotes()">👍</button>
+        </ul>`
+  );
+}
+document.getElementById("cours").innerHTML = codeAinserer;
+
+function updateVotes() {
+  console.log("Vote incrémenté");
+}
